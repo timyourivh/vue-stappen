d19babcf2d9f8222de4fb1f777aa0b34348015de
Tim van Herwijnen <t.vanherwijnen@helmink.nl>
2023-07-04 16:26:07 +0200

    Fix broken eslint and updated style

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,19 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
   ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
   },
   rules: {
-    "no-unused-vars": ["error", { args: "none" }],
-    "vue/multi-word-component-names": false,
+    'no-unused-vars': ['error', { args: 'none' }],
+    'vue/multi-word-component-names': ['off'],
   },
 };
