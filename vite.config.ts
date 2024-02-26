f294ad079b546aaf7fe022b1a7e3b6cac7ad58ee
Tim van Herwijnen <t.vanherwijnen@helmink.nl>
2024-02-26 17:22:05 +0100

    Prepare for publishing

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    dts(),
   ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/components/export/index.ts"),
+      name: "VueStappen",
+      fileName: "vue-stappen",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
